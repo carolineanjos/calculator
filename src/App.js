2456676bf1acdcc159df014301dc8e6030547d97
Caroline <carolineanjoss02@gmail.com>
2023-01-25 18:00:08 -0300

    starting calculator's project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const number = [1,2,3,4,5,6,7,8,9]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="leftSide">
+        <div></div>
+        <div className="numbers">
+          {number.map((val, key) => {
+           return <div id="individualNumber">{val}</div>
+          })}
+        </div>
+        </div>
+        <div className="RightSide"></div>
+      </div>
     </div>
   );
 }
